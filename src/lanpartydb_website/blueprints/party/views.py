4df0e30cfed25105be03e6b95e96927a2e7e83eb
Jochen Kupperschmidt <homework@nwsnet.de>
2026-03-05 00:04:24 +0100

    Reformat code

--- src/lanpartydb_website/blueprints/party/views.py
+++ src/lanpartydb_website/blueprints/party/views.py
@@ -73,8 +73,9 @@ def index_by_country(country_code: str, page: int):
 
     parties = _get_parties()
     parties = filter(
-        lambda party: _find_party_country_code(party)
-        == country.alpha_2.lower(),
+        lambda party: (
+            _find_party_country_code(party) == country.alpha_2.lower()
+        ),
         parties,
     )
     parties = list(
